1dd5410eb0c9c68637e2f966a1ffac0c3cc1ea56
SrFemix <68878209+SrFemix@users.noreply.github.com>
2024-09-07 12:15:52 -0300

    Projeto ImersãoDev_

---
+++ app.js
@@ -0,0 +1,50 @@
+function pesquisar() {
+
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value 
+
+    //se campoPesquisa for uma string sem nada :
+
+    if (!campoPesquisa) {
+    section.innerHTML = "<b>Nenhum Resultado Encontrado</b>"
+    
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada elemento (dado) da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            //cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <b <p><a href="${dado.link}" target="_blank"> Mais informações</a></p></b>
+            </div>
+            `;
+        }
+
+    if (!resultados){
+        resultados = "<b>Busca Não Encontrada</b>"
+    }
+
+    }
+
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+}
+
