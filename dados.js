794c54c2fe97127b6f5193f19bf4895325e150db
SrFemix <68878209+SrFemix@users.noreply.github.com>
2024-09-07 12:53:17 -0300

    dados.js

--- dados.js
+++ dados.js
@@ -1,33 +1,33 @@
 let dados = [
-    {
+        {
         titulo: "Google Gemini",
         descricao: "O Gemini é uma ferramenta incrível que usa inteligência artificial para responder às suas perguntas, te ajudar com tarefas e até mesmo te ensinar coisas novas! É como ter um superpoderoso parceiro de estudos e aventuras!",
         link: "https://gemini.google.com/app?hl=pt-BR",
-        tags: "inteligência inteligencia artificial assistente virtual aprendizado de máquina IA AI Google ferramenta de aprendizado"
+        tags: "inteligência artificial assistente virtual aprendizado máquina IA Google ferramenta aprendizado"
     },
     {
         titulo: "HTML",
         descricao: "O HTML é como essas peças para as páginas da internet. Ele define a estrutura básica de um site, dizendo onde vai o título, as fotos, os parágrafos e tudo mais que você vê em uma página. É como a base de uma casa, onde você vai colocar os móveis e decorar. O HTML é a linguagem que os computadores entendem para mostrar as páginas da web da forma que a gente está acostumado a ver!",
         link: "https://pt.wikipedia.org/wiki/HTML",
-        tags: "HTML linguagem de marcação marcacao estrutura de página pagina desenvolvimento web criação criacao sites web"
+        tags: "HTML linguagem marcação estrutura de página desenvolvimento web criação sites web"
     },
     {
         titulo: "CSS",
         descricao: "O CSS é como a decoração de um site! Ele define a aparência de uma página web, tornando-a bonita e organizada. Com o CSS, você pode escolher as cores das letras, o tamanho das imagens e até mesmo criar efeitos legais, como animações. É como dar um toque especial na sua página, deixando-a do jeitinho que você quiser!",
         link: "https://pt.wikipedia.org/wiki/Cascading_Style_Sheets",
-        tags: "CSS estilo design web estilização estilizacao layout interface"
+        tags: "CSS folhas estilo design web estilização layout interface"
     },
     {
         titulo: "JavaScript",
         descricao: "O JavaScript é como a mágica que faz com que seu castelo de areia se mova, faça barulhos e até mesmo conte histórias! É a parte divertida da programação que transforma páginas estáticas em aventuras interativas. Com o JavaScript, você pode criar jogos, animações e fazer com que os sites respondam aos seus cliques e toques, tornando a internet um lugar muito mais legal e divertido de explorar!",
         link: "https://pt.wikipedia.org/wiki/JavaScript",
-        tags: "JavaScript programação programacao desenvolvimento web interatividade frontend front end criação criacao jogos"
+        tags: "JavaScript programação desenvolvimento web interatividade frontend criação jogos"
     },
     {
         titulo: "Lógica de Programação",
         descricao: "A lógica de programação é como um guia de instruções para um computador. Imagine você ensinando um cachorro a fazer um truque: você precisa dar comandos claros e na ordem certa. A lógica de programação faz a mesma coisa, só que com um computador! Ela ensina o computador a realizar tarefas específicas, como fazer cálculos, mostrar imagens na tela ou até mesmo jogar um jogo. É como criar uma receita para que o computador possa seguir passo a passo e realizar uma tarefa.",
-        link: "https://www.rocketseat.com.br/blog/artigos/post/logica-de-programação-para-iniciantes-em-programacao#:~:text="
-        + "logica-de-programacao-para-iniciantes-em-programacao",
-        tags: "lógica programação algoritmos pensamento computacional programacao básica basica desenvolvimento software"
+        link: "https://www.rocketseat.com.br/blog/artigos/post/logica-de-programacao-para-iniciantes-em-programacao#:~:text=A%20lógica%20de%20programação%20é%20o%20conjunto%20de%20regras%20e,complexo%20em%20etapas%20mais%20simples.",
+        tags: "lógica programação algoritmos pensamento computacional programação básica desenvolvimento software"
     }
+
 ];
